d735e62f0f5b51b10796c69bcf77aadbf4530430
Felipe <felipealvescosta@live.com>
2021-09-25 12:32:53 -0300

    chore: update auth file

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,9 +1,9 @@
 import React, { createContext, ReactNode, useState, useContext } from "react";
 
 const {CLIENT_ID} = process.env;
-const {REDIRECT_URI} = process.env;
+const { REDIRECT_URI } = process.env;
 
-import * as AuthSession from 'expo-auth-session';
+import * as AuthSession from "expo-auth-session";
 
 import { User } from "../screens/Dashboard/styles";
 
@@ -37,17 +37,20 @@ function AuthProvider({ children }: AuthProviderProps) {
 
   async function signInWithGoogle() {
     try {
-      const RESPONSE_TYPE = 'token';
-      const SCOPE = encodeURI('profile email');
+      const RESPONSE_TYPE = "token";
+      const SCOPE = encodeURI("profile email");
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
-      
-      const {type, params}  =  await AuthSession
-        .startAsync({ authUrl}) as AuthorizationResponse ; 
 
-      if(type === 'success'){
-        const response =  await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
-        const userInfo =  await response.json();
-        
+      const { type, params } = (await AuthSession.startAsync({
+        authUrl,
+      })) as AuthorizationResponse;
+
+      if (type === "success") {
+        const response = await fetch(
+          `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`
+        );
+        const userInfo = await response.json();
+
         setUser({
           id: userInfo.id,
           email: userInfo.email,
@@ -57,7 +60,6 @@ function AuthProvider({ children }: AuthProviderProps) {
 
         console.log(user);
       }
-
     } catch (error: any) {
       throw new Error(error);
     }
